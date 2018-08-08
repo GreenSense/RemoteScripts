3d6eb70c34a70908674855f5364d2fd500e12b35
CompulsiveCoder <compulsivecoder@gmail.com>
2018-08-08 05:30:10 +0000

    Fixed console output

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
 echo "Preparing GreenSense remote scripts"
-echo "Installing required software via ap-get..."
+echo "Installing required software via apt-get..."
 
 sudo apt-get update && sudo apt-get -y install fping sshpass
